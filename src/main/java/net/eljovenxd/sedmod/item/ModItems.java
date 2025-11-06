cc1c221fd7f10643bee45f1717b5740fb78eac1b
Jose de Jesus Martin Zuniga <eviljjmz@gmail.com>
2025-11-06 09:12:02 -0600

    Discos puestos y texturas

--- src/main/java/net/eljovenxd/sedmod/item/ModItems.java
+++ src/main/java/net/eljovenxd/sedmod/item/ModItems.java
@@ -141,13 +141,13 @@ public class ModItems {
             () -> new ItemNameBlockItem(ModBlocks.LECHUGA_CROP.get(), new Item.Properties()));
 
     public static final RegistryObject<Item> WARRIORS_MUSIC_DISC = ITEMS.register("warriors_music_disc",
-            () -> new RecordItem(6, ModSounds.WARRIORS, new Item.Properties().stacksTo(1), 2440));
+            () -> new RecordItem(6, ModSounds.WARRIORS, new Item.Properties().stacksTo(1), 3160));
 
     public static final RegistryObject<Item> RISE_MUSIC_DISC = ITEMS.register("rise_music_disc",
-            () -> new RecordItem(6, ModSounds.RISE, new Item.Properties().stacksTo(1), 2440));
+            () -> new RecordItem(6, ModSounds.RISE, new Item.Properties().stacksTo(1), 3840));
 
     public static final RegistryObject<Item> LIGHT_AND_SHADOW_MUSIC_DISC = ITEMS.register("light_and_shadow_music_disc",
-            () -> new RecordItem(6, ModSounds.LIGHT_AND_SHADOW, new Item.Properties().stacksTo(1), 2440));
+            () -> new RecordItem(6, ModSounds.LIGHT_AND_SHADOW, new Item.Properties().stacksTo(1), 5300));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
